ce3220c080bd8ce85af8e8e39978ef4eeeeef7f3
young8903305 <young8903305@gmail.com>
2016-10-02 23:37:22 +0800

    init

---
+++ hw1/hw1_105753024.R
@@ -0,0 +1,22 @@
+# read PAM1 from data
+pam1<-read.table("pam1.txt",header = TRUE)    #read file without seperate
+
+# check PAM1 data
+dim(pam1)    # show/set the dimension
+str(pam1)    # show the structure
+
+
+# construct PAM250 from PAM1
+multiple  <- function(matrix, n){    # function of matrix multiply
+  if(n == 1)
+    return (matrix)
+  else
+    return (matrix %*% multiple(matrix, n-1))
+}
+
+p <- as.matrix( pam1 ) / 10000    # change pam1 data.frame into matrix
+p250 <- round( multiple(p, 250) * 100, digits = 0 )    # result * 100, then round to 小數點前一位
+
+# output PAM250 as a file
+write.table(p250, file = "pam250.txt", sep = "\t", quote = FALSE, append = FALSE, na = "NA")    #seperate with \t, no quote, can overwrite the same file
+
